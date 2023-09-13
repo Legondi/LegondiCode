8272d84651de632fa89660fb4827aae98e210d17
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-13 23:44:23 +0000

    att

--- Exercicio_A.java
+++ Exercicio_A.java
@@ -3,7 +3,7 @@
 
         public static void main(String[] args) {
         
-            for (int i = 0; i <= 50; i++) {
+            for (int i = 1; i <= 50; i++) {
                 
                 System.out.println(i);
             }
