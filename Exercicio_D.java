9bfab49aa2f50724a02da13148ccfb9a6adeea9f
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-14 01:00:51 +0000

    att

---
+++ Exercicio_D.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+ public class Exercicio_D {
+
+    public static void main(String [] args) {
+
+     Scanner entrada = new Scanner(System.in);
+
+     int valor = -1;
+
+     int maior = 0;
+
+     int menor = 0;
+
+        while (valor != 0) {
+                
+        System.out.println("Digite um Numero: ");
+       
+        valor = entrada.nextInt();
+
+        if (maior < valor){ 
+            maior = valor;}
+
+          System.out.println("maior numero é: " + maior);  
+
+        }
+
+    }
+
+}
