6da07546c0f468d364ac4a5759fa336e085dfd6c
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-18 01:32:26 +0000

    study

---
+++ booleans.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class booleans{
+
+    public static void main(String[] args){
+
+        boolean 七_Qī7 = true;
+
+        int 七_Qī = 7;
+
+        Scanner 凌_Líng = new Scanner (System.in);
+        
+        System.out.println("Digite o valor de 七_Qī");
+        七_Qī = 凌_Líng.nextInt();
+
+        
+        
+        if (七_Qī == 7) {
+            
+            七_Qī7 = true;
+            
+            System.out.println("我_Wǒ acertou!!");}
+               
+        else {
+
+            七_Qī7 = false;
+
+            System.out.println ("我_Wǒ errou!!");
+        }
+
+        
+      凌_Líng.close();  
+        
+
+
+    }
+
+
+}
