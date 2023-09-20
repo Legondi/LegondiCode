b8fe2fe0b2fe2baf52870da18e11606c9a74e00c
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-20 01:06:54 +0000

    aaaaa

---
+++ switchh.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+    
+    public class switchh {
+
+        public static void main (String [] args) {
+
+        int teste = 0;
+        
+        Scanner caso = new Scanner (System.in);
+
+    }
+    
+}
