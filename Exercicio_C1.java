d2bff2c8b932d2fbb46e5c1adfe4c6fdb7316d42
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-26 01:29:35 +0000

    aa

---
+++ Exercicio_C1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+    public class Exercicio_C1 {
+    
+        public static void main (String [] args) {
+
+            Scanner att = new Scanner (System.in);
+
+             int x;
+
+             int y;
+
+             int z = 0;
+
+             int w = 0;
+
+            System.out.print("Digite o valor de X: "); 
+             x = att.nextInt();
+
+            System.out.print("Digite o valor de Y: "); 
+             y = att.nextInt();
+
+            System.out.println("valores de X e Y em respectiva ordem: ");
+            System.out.println("X: " + x);
+            System.out.println("Y: " + y); 
+
+            if (x != y) {z = x;
+                         w = y;
+                         x = w;
+                         y = z;
+                         
+             System.out.println("inversão de valores!: " + x);
+             System.out.println("inversão de valores!: " + y);}
+
+            else if (x == y){System.out.println("valores iguais: " + y);}             
+
+
+    
+    
+    
+        }
+    
+
+}
