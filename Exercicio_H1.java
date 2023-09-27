741ff64c229c90fba81f2cfb3a36890eca30e15d
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-27 22:57:49 +0000

    aa

--- Exercicio_H1.java
+++ Exercicio_H1.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Exercicio_H1 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho do lado do quadrado (entre 1 e 20):");
+        
+        int tamanholado = entrada.nextInt();
+
+        if (tamanholado < 1 || tamanholado > 20) {
+            System.out.println("Tamanho fora do intervalo permitido.");
+        } else {
+            
+            for (int i = 0; i < tamanholado; i++) {
+                for (int j = 0; j < tamanholado; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+
+
+    }
     
+ }
+
+  entrada.close();
+ 
+}
+
 }
--- Exercicio_I1.java
+++ Exercicio_I1.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Exercicio_I1 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho do triângulo: ");
+
+        int tamanho = entrada.nextInt();
+
+        for (int i = 1; i <= tamanho; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+      
+        entrada.close();
+
+    }
     
 }
