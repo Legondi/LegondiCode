4c6f5c406df0328680582550936f1acaff9495fd
Legondi <143142229+Legondi@users.noreply.github.com>
2023-09-27 22:41:38 +0000

    aaa

--- Exercicio_K1.java
+++ Exercicio_K1.java
@@ -1,31 +1,61 @@
 import java.util.Scanner;
 
 public class Exercicio_K1 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] vetor = new int[10];
-        int indice = 0;
+        while (true) {
+            System.out.println("Selecione a operação:");
+            System.out.println("1. Adição");
+            System.out.println("2. Subtração");
+            System.out.println("3. Multiplicação");
+            System.out.println("4. Divisão");
+            System.out.println("5. Sair");
 
-        System.out.println("Digite os números ímpares para preencher o vetor:");
+            int escolha = scanner.nextInt();
 
-        while (indice < 10) {
-            System.out.print("Digite um número ímpar: ");
-            int numero = scanner.nextInt();
+            if (escolha == 5) {
+                System.out.println("Saindo do programa.");
+                break;
+            }
 
-            if (numero % 2 != 0) {
-                vetor[indice] = numero;
-                indice++;
-            } else {
-                System.out.println("Esse número não é ímpar. Tente novamente.");
+            if (escolha < 1 || escolha > 4) {
+                System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                continue;
+            }
+
+            System.out.print("Digite o primeiro número: ");
+            double num1 = scanner.nextDouble();
+            System.out.print("Digite o segundo número: ");
+            double num2 = scanner.nextDouble();
+
+            double resultado = 0;
+
+            switch (escolha) {
+                case 1:
+                    resultado = num1 + num2;
+                    System.out.println("Resultado da adição: " + resultado);
+                    break;
+                case 2:
+                    resultado = num1 - num2;
+                    System.out.println("Resultado da subtração: " + resultado);
+                    break;
+                case 3:
+                    resultado = num1 * num2;
+                    System.out.println("Resultado da multiplicação: " + resultado);
+                    break;
+                case 4:
+                    if (num2 == 0) {
+                        System.out.println("Erro: divisão por zero");
+                    } else {
+                        resultado = num1 / num2;
+                        System.out.println("Resultado da divisão: " + resultado);
+                    }
+                    break;
             }
         }
 
         scanner.close();
-
-        System.out.print("Vetor de números ímpares: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(vetor[i] + " ");
-        }
     }
 }
