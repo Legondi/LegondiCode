91dcfe7b77f346b2e4c179ce0008811da24a2337
Legpndi <leogdias12@gmail.com>
2025-03-07 20:11:58 -0300

    Comitando arquivos antigos

---
+++ python/print.py
@@ -0,0 +1,11 @@
+raio = float (input('Informe o raio(cm): '))
+
+area = 3.14159 * raio ** 2
+
+print ('A área do círculo é', area, 'cm²')
+
+area2 = raio*raio*3.14159 
+
+print ('A área do círculo é', area2, 'cm²')
+
+print (3.14159 * raio ** 2)
