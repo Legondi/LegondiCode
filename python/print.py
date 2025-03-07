4ea41589574ddbe5d194da44b1ffbf5e165f7166
Legpndi <leogdias12@gmail.com>
2025-03-07 20:31:47 -0300

    save

--- python/print.py
+++ python/print.py
@@ -1,3 +1,6 @@
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
 """ raio = float (input('Informe o raio(cm): '))
 
 area = 3.14159 * raio ** 2
@@ -11,10 +14,29 @@ print ('A área do círculo é', area2, 'cm²')
 print (3.14159 * raio ** 2)
 
  """
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
  
-number1 = int(input('Digite o primeiro número: '))
+ 
+""" number1 = int(input('Digite o primeiro número: '))
 number2 = int(input('Digite o segundo número: '))
 
 multiplication = number1 * number2 
 
-print ('A multiplicação dos números é', multiplication)
+print ('A multiplicação dos números é', multiplication) """
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
+base = float(input('Digite a base do triângulo: '))
+
+altura = float(input('digite a altura do triângulo: '))
+
+area = base * altura / 2 
+
+print ('A área do triângulo é', area, 'cm²')
+
+
+
