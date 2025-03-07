4abe5b25a886836dc411235d5b2d8b2af97a1e0c
Legpndi <leogdias12@gmail.com>
2025-03-07 20:23:07 -0300

    save

--- python/print.py
+++ python/print.py
@@ -1,4 +1,4 @@
-raio = float (input('Informe o raio(cm): '))
+""" raio = float (input('Informe o raio(cm): '))
 
 area = 3.14159 * raio ** 2
 
@@ -9,3 +9,12 @@ area2 = raio*raio*3.14159
 print ('A área do círculo é', area2, 'cm²')
 
 print (3.14159 * raio ** 2)
+
+ """
+ 
+number1 = int(input('Digite o primeiro número: '))
+number2 = int(input('Digite o segundo número: '))
+
+multiplication = number1 * number2 
+
+print ('A multiplicação dos números é', multiplication)
