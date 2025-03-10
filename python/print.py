22f676ae37b0bdbd2028ea5327a453661c216548
Legondi <leogdias12@gmail.com>
2025-03-10 20:31:07 -0300

    save

--- python/print.py
+++ python/print.py
@@ -30,11 +30,68 @@ print ('A multiplicação dos números é', multiplication) """
 #龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
 
 
-base = float(input('Digite a base do triângulo: '))
+""" base = float(input('Digite a base do triângulo: '))
 
 altura = float(input('digite a altura do triângulo: '))
 
 area = base * altura / 2 
 
-print ('A área do triângulo é', area, 'cm²')
+print ('A área do triângulo é', area, 'cm²') """
 
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
+# media = float(input('Digite sua nota: '))
+# media2 = float(input('Digite sua nota: '))
+
+# resultado = (media + media2) / 2
+
+# print ('sua media é', resultado)
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
+# temp = float(input('digite quantos graus está para converter em fahrenheit: '))
+
+# result = (9*temp + 160) / 5
+
+# print ('A temperatura em fahrenheit é', result, '°F')
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
+# raiz = float(input('Digite um número para saber a raiz quadrada: '))
+
+# result = raiz ** 0.5
+
+# print ('A raiz quadrada de', raiz, 'é', result)
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
+# comp = float(input('digite o comprimento: '))
+# larg = float(input('digite a largura: '))
+# alt = float(input('digite a altura: '))
+
+# result = comp * larg * alt
+
+# print ('O volume é', result, 'm³')
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+
+
+# var1 = int(input('Digite o valor 1: '))
+# var2 = int(input('Digite o valor 2: '))
+
+# var3 = var1
+# var1 = var2
+# var2 = var3
+
+# print ('o valor1 agora é', var1, 'e o valor2 agora é', var2)
+
+
+#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
