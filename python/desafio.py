b63016f5dbcd86a7459532ea46e3209581f2604f
Legondi <leogdias12@gmail.com>
2025-04-17 09:30:44 -0300

    repair 4

--- python/desafio.py
+++ python/desafio.py
@@ -1,28 +1,3 @@
-#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
-
-
-
-""" Desafio: pesquisar uma maneira do usuário informar a data de nascimento
-
-e calcular a idade deste usuário
-
- 
-
-Coloque o código no fórum e faça comentários do código pesquisado """
-
-
-
-#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
-
-
-""" Neste algoritmo tentei utilizar somente operações e funções apresentadas durante a aula
-para não fugir do escopo lógico do desafio. Irá apresentar idade, meses e dias vividos com
-uma margem de erro para anos bissextos. """
-
-
-#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
-
-
 def calcular_idade(ano_nasc, mes_nasc, dia_nasc, ano_atual, mes_atual, dia_atual):
     atual_total = ano_atual * 365 + mes_atual * 30 + dia_atual
     nascimento_total = ano_nasc * 365 + mes_nasc * 30 + dia_nasc
@@ -35,42 +10,21 @@ def calcular_idade(ano_nasc, mes_nasc, dia_nasc, ano_atual, mes_atual, dia_atual
     return {"anos": anos, "meses": meses, "dias": dias}
 
 
+def main():
+    mes_nascimento = int(input("Informe apenas o seu mês de nascimento (número): "))
+    dia_nascimento = int(input("Informe apenas o seu dia de nascimento (número): "))
+    ano_nascimento = int(input("Informe apenas o seu ano de nascimento (número): "))
 
-mes_nascimento = int(input("Informe apenas o seu mês de nascimento (número): "))
-dia_nascimento = int(input("Informe apenas o seu dia de nascimento (número): "))
-ano_nascimento = int(input("Informe apenas o seu ano de nascimento (número): "))
+    print("Você nasceu em: ", dia_nascimento, "/", mes_nascimento, "/", ano_nascimento)
 
+    mes_atual = int(input("Informe apenas o mês atual (número): "))
+    dia_atual = int(input("Informe apenas o dia atual (número): "))
+    ano_atual = int(input("Informe apenas o ano atual (número): "))
 
-print("Você nasceu em: ", dia_nascimento, "/", mes_nascimento, "/", ano_nascimento)
+    idade = calcular_idade(ano_nascimento, mes_nascimento, dia_nascimento, ano_atual, mes_atual, dia_atual)
 
-
-mes_atual = int(input("Informe apenas o mês atual (número): "))
-dia_atual = int(input("Informe apenas o dia atual (número): "))
-ano_atual = int(input("Informe apenas o ano atual (número): "))
-
-atual_total = ano_atual * 365 + mes_atual * 30 + dia_atual
-nascimento_total = ano_nascimento * 365 + mes_nascimento * 30 + dia_nascimento
-dias_vividos = atual_total - nascimento_total
-
-idade1 = dias_vividos / 365
-
-simplificarA = int(idade1)
-
-idade2 = idade1 - simplificarA
-
-idadeMes = idade2 * 12
-
-simplificarM = int(idadeMes)
-
-idade3 = idadeMes - simplificarM
-
-idadeDia = idade3 * 30
-
-simplificarD = int(idadeDia)
-
-#sujeito a erros de calculo, pois não foi levado em consideração os anos bissextos
-print("Você tem: ", simplificarA, "anos", simplificarM, "meses", simplificarD, "dias")
+    print("Você tem: ", idade["anos"], "anos", idade["meses"], "meses", idade["dias"], "dias")
 
 
-
-#龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙龙
+if __name__ == "__main__":
+    main()
--- tests/test_desafio.py
+++ tests/test_desafio.py
@@ -3,3 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../python")))
 
 from desafio import calcular_idade
+
+def test_calculo_simples():
+    resultado = calcular_idade(2000, 1, 1, 2025, 1, 1)
+    assert resultado["anos"] == 25
